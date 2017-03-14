9a174d67d6fecc424b8dbb4eff513cb4481151b3
androsh <apanasiuk00@gmail.com>
2017-03-14 10:49:09 +0000

    got rid of forced reflow in main.js, perf ~60fps

--- views/js/main.js
+++ views/js/main.js
@@ -502,8 +502,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var scroll = document.body.scrollTop;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((scroll / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
